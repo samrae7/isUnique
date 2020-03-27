f0dd8735938ce3505eec18364d9087105d3bd2cf
Sam Rae <samuelrae@gmail.com>
2020-03-27 07:54:17 +0000

    initial commit

---
+++ IsUnique.java
@@ -0,0 +1,21 @@
+import java.util.Hashtable;
+
+public class IsUnique {
+  public static void main(final String[] args) {
+    final Boolean result = isUnique("fyuxxxtrw");
+    System.out.println("Result: " + result);
+  }
+
+  private static Boolean isUnique(final String word) {
+    final Hashtable<Character, Boolean> hash_table = new Hashtable<Character, Boolean>();
+    final char[] chars = word.toCharArray();
+    for (final char w : chars) {
+      if (hash_table.get(w) != null) {
+        return false;
+      } else {
+        hash_table.put(w, true);
+      }
+    }
+    return true;
+  }
+}
